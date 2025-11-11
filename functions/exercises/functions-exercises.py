54b282c0097e09bbbd3858e2747d55b17444b50d
JHenke <jwhenke08@gmail.com>
2025-11-11 17:32:05 -0600

    Functions exercise

--- functions/exercises/functions-exercises.py
+++ functions/exercises/functions-exercises.py
@@ -1,41 +1,78 @@
 # Part 1 A -- Make a Line
+def make_line(size):
+    line = ''
+    for i in range(size):
+        line += "#"
+    return line + "\n"
 
-
+print(make_line(5))
 
 # Part 1 B -- Make a Square
 # create a function using your make_line function to code a square
 
+def make_square(size):
+    square =''
+    for i in range(size):
+        square += make_line(size)
+    return square
 
-
-
+print(make_square(5))
 
 # Part 1 C -- Make a Rectangle
+def make_rectangle(width,height):
+    rectangle =''
+    for i in range(height):
+        rectangle += make_line(width)
+    return rectangle
 
-
-
-
+print(make_rectangle(5,3))
+    
 
 # Part 2 A -- Make a Stairs
+def make_downward_stairs(height):
+    stairs =''
+    for i in range(height):
+        stairs += make_line(i+1)
+    return stairs
 
-
-
-
+print(make_downward_stairs(5))
 
 # Part 2 B -- Make Space-Line 
+def make_space_line(numSpaces, numChars):
+    space_line =''
+    for i in range(numSpaces):
+        space_line += " "
+    for i in range(numChars):
+        space_line += "#"
+    for I in range(numSpaces):
+        space_line += " "
+    return space_line
 
-
-
+print(make_space_line(3,5))
 
 
 # Part 2 C -- Make Isosceles Triangle
+def make_isosceles_triangle(height):
+    triangle = ''
+    for i in range(height):
+        triangle += (make_space_line(height-i-1, 2 * i + 1) + '\n')
+    return triangle
 
-
-
+print('\n')
+print(make_isosceles_triangle(5))      
 
 
 # Part 3 -- Make a Diamond
+def make_diamond(height):
+    diamond=''
+    triangle = make_isosceles_triangle(height)
+    diamond += triangle[:-1]
+    for i in range(len(triangle)-1, -1, -1):
+        diamond += triangle[i]
+    return diamond
 
-
+print('\n')
+print(make_diamond(5))
 
 
 
